09f0cc45769f456dd321f6ca43f2c7abf7fc5b02
IanGustavo15 <98957131+IanGustavo15@users.noreply.github.com>
2025-10-13 18:04:11 -0300

    fim d010?

---
+++ desafios/d010/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculando sua idade</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <main>
+        <h1>Calculando a sua idade</h1>
+        <form action="<?=$_SERVER["PHP_SELF"]; $ano_atual=date("Y")?>" method="get">
+            <label for="ano_nascimento">Em que ano você nasceu?</label>
+            <input type="number" name="ano_nascimento" id="id_ano_nascimento" step="1">
+            <label for="ano_futuro">Quer saber sua idade em que ano? (atualmente estamos em <?=$ano_atual?>)</label>
+            <input type="number" name="ano_futuro" id="ano_futuro" step="1">
+            <input type="submit" value="Qual será a minha idade?">
+        </form>
+        </main>
+        <section>
+            <h2>Resultado</h2>
+        <?php 
+        $ano_nascimento = $_GET["ano_nascimento"] ?? 2000;
+        $ano_futuro = $_GET["ano_futuro"] ?? $ano_atual;
+        $idade = (int)$ano_futuro - (int)$ano_nascimento;
+        echo "Quem nasceu em $ano_nascimento vai ter <strong>$idade anos</strong> em $ano_futuro!";
+        ?>
+        </section>
+</body>
+</html>
