a3652081153ca81827f2d755f1d97521359422f3
IanGustavo15 <98957131+IanGustavo15@users.noreply.github.com>
2025-10-13 10:11:53 -0300

    fim d004

--- desafios/d003/cad.php
+++ desafios/d003/cad.php
@@ -19,7 +19,7 @@
             $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
             echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL")  . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong></p>";
-            echo "<p><strong>*Cotação fixa de R\$ " . numfmt_format_currency($padrao, $cotacao, "BRL") . "</strong> informada diretamente no código.</p>";
+            echo "<p><strong>*Cotação fixa de " . numfmt_format_currency($padrao, $cotacao, "BRL") . "</strong> informada diretamente no código.</p>";
         ?>
         <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
     </section>
