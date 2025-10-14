b7e8f6a67d4299ce2eb59bf2eac4051714f12267
IanGustavo15 <98957131+IanGustavo15@users.noreply.github.com>
2025-10-14 14:48:05 -0300

    fim d012

--- desafios/d012/index.php
+++ desafios/d012/index.php
@@ -8,14 +8,26 @@
 </head>
 <body>
     <?php 
-        $total_segundos = (int)$_GET["total_segundos"] ?? 0;
-        
+        $t_semana = 604800;
+        $t_dia = 86400;
+        $t_hora = 3600;
+        $t_minuto = 60;
+        $resto = 0;
+        $total_segundos = $_GET["total_segundos"] ?? 0;
+        $semanas = $total_segundos / $t_semana;
+        $resto = $total_segundos % $t_semana;
+        $dias =  $resto / $t_dia;
+        $resto = $resto % $t_dia;
+        $horas = $resto / $t_hora;
+        $resto = $resto % $t_hora;
+        $minutos = $resto / $t_minuto;
+        $resto = $resto % $t_minuto;
     ?>
     <main>
         <h1>Calculadora de Tempo</h1>
         <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
             <label for="total_segundos">Qual é o total de segundos?</label>
-            <input type="number" name="total_segundos" id="id_total_segundos" value="<?=$total_segundos?>" step="1">
+            <input type="number" name="total_segundos" id="id_total_segundos" value="<?=$total_segundos?>" step="1" min="0">
             <input type="submit" value="Calcular">
         </form>
     </main>
@@ -23,12 +35,12 @@
         <h2>Totalizando tudo</h2>
         <?php 
         echo "Analisando o tempo que você digitou, <strong>". number_format($total_segundos, 0, ",", ".") . " segundos</strong> equivalem a um total de: ";
-        // echo "<ul>
-        // <li>$int_semanas semanas</li>
-        // <li>$int_dias dias</li>
-        // <li>$int_horas horas</li>
-        // <li>$int_minutos minutos</li>
-        // <li>$int_segundos segundos</li>"
+        echo "<ul>
+        <li>".floor($semanas). " semanas</li>
+        <li>". floor($dias) . " dias</li>
+        <li>". floor($horas) . " horas</li>
+        <li>" . floor($minutos) . " minutos</li>
+        <li>". $resto ." segundos</li>"
         ?>
     </section>
 </body>
