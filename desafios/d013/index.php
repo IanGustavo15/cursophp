f6c9bf327075c503351cec1e917dc38f22f8edc0
IanGustavo15 <98957131+IanGustavo15@users.noreply.github.com>
2025-10-15 17:55:18 -0300

    fim d013

--- desafios/d013/index.php
+++ desafios/d013/index.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Caixa Eletrônico</title>
     <link rel="stylesheet" href="../style.css">
+    <style>
+        img.nota{
+            height: 80px;
+            margin: 5px;
+        }
+    </style>
 </head>
 <body>
     <?php 
@@ -13,17 +19,17 @@
         $nota_100 = $saque / 100;
         $resto = $saque  % 100;
         $nota_50 =  $resto / 50;
-        $resto = $resto  % 50;
+        $resto %= 50;
         $nota_10 = $resto / 10;
-        $resto = $resto  % 10;
+        $resto %= 10;
         $nota_5 = $resto / 5;
     ?>
     <main>
         <h1>Caixa Eletrônico</h1>
         <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
-        <label for="saque">Qual valor você deseja sacar? (R$)*</label>
-        <input type="number" name="saque" id="saque" value="<?=$saque?>" step="5">
-        <p>Notas disponíveis: R$100, R$50, R$10, R$5</p>
+        <label for="saque">Qual valor você deseja sacar? (R$)<sup>*</sup></label>
+        <input type="number" name="saque" id="saque" value="<?=$saque?>" step="5" required>
+        <p style="font-size: 0.7em"><sup>*</sup>Notas disponíveis: R$100, R$50, R$10, R$5</p>
         <input type="submit" value="Sacar">
         </form>
     </main>
@@ -31,10 +37,10 @@
         <h2>Saque de <?=numfmt_format_currency($padrao, $saque, "BRL")?></h2>
         <p>O caixa eletrônico vai te entregar as seguintes notas: </p>
        <ul>
-        <li><img src="imagens/100-reais.jpg" alt="nota100"><?="x".floor($nota_100)?></li>
-        <li><img src="imagens/50-reais.jpg" alt="nota100"><?="x".floor($nota_50)?></li>
-        <li><img src="imagens/10-reais.jpg" alt="nota100"><?="x".floor($nota_10)?></li>
-        <li><img src="imagens/5-reais.jpg" alt="nota100"><?="x".floor($nota_5)?></li>
+        <li><img src="imagens/100-reais.jpg" alt="nota100" class="nota"><?="x".floor($nota_100)?></li>
+        <li><img src="imagens/50-reais.jpg" alt="nota50" class="nota"><?="x".floor($nota_50)?></li>
+        <li><img src="imagens/10-reais.jpg" alt="nota10" class="nota"><?="x".floor($nota_10)?></li>
+        <li><img src="imagens/5-reais.jpg" alt="nota5" class="nota"><?="x".floor($nota_5)?></li>
        </ul>
     </section>
 </body>
